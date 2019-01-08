d63fe76eead6ed4667759d45573c12f60e89c0b6
Raghu Betina <raghu@firstdraft.com>
2019-01-08 22:27:15 +0000

    Generate actors resource with draft:resource

---
+++ db/migrate/20190108222715_create_actors.rb
@@ -0,0 +1,12 @@
+class CreateActors < ActiveRecord::Migration[5.1]
+  def change
+    create_table :actors do |t|
+      t.string :name
+      t.date :dob
+      t.text :bio
+      t.string :roles
+
+      t.timestamps
+    end
+  end
+end
