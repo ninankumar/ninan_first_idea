2eef78d7eff0bdb64a592c9ec9cc366f8dbb4063
Raghu Betina <raghu@firstdraft.com>
2019-01-08 22:28:21 +0000

    Whitelist columns in Role for ActiveAdmin

--- app/admin/roles.rb
+++ app/admin/roles.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Role do
+
+ permit_params :movied_id, :actors_id, :characters_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
